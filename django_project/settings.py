f3a0ba8713e16777901e8374771cd41d5148dc9f
ttrivedi.1999@gmail.com <57986361+tejas-trivedi@users.noreply.github.com>
2020-01-18 00:55:43 +0530

    Update

--- django_project/settings.py
+++ django_project/settings.py
@@ -78,10 +78,15 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'djdb',
+        'USER': 'postgres',
+        'PASSWORD': 'COM@2307',
+        'HOST': '127.0.0.1',
+        'POST': '5432'
     }
 }
+
 
 
 # Password validation
